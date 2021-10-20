8c3a6d206458c2391094943a02e43a0eaa89fe93
tim <tim.hladnik@gmail.com>
2021-10-20 18:17:36 +0200

    Move start script to __main__ for now; fix import in Default.py;

--- mappapp/__main__.py
+++ mappapp/__main__.py
@@ -15,8 +15,77 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
+import argparse
+import sys
+from PyQt5 import QtWidgets
+from vispy import app, gloo
+
+from mappapp.modules import Controller
+from mappapp.setup import acc
+
 
 if __name__ == '__main__':
-    from mappapp.setup import run
 
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ini', action='store', dest='ini_file', type=str)
+    args = parser.parse_args(sys.argv[1:])
+
+    from mappapp.setup.main import StartupConfiguration
+
+    if sys.platform == 'win32':
+
+        app.use_app('PyQt5')
+        gloo.gl.use_gl('gl2')
+
+        import wres
+
+        # Set windows timer precision as high as possible
+        minres, maxres, curres = wres.query_resolution()
+        with wres.set_resolution(maxres):
+
+            configfile = None
+
+            if args.ini_file is not None:
+                configfile = args.ini_file
+
+            else:
+
+                acc.app = QtWidgets.QApplication([])
+                acc.main = StartupConfiguration()
+                acc.main.setup_ui()
+                acc.app.exec_()
+
+                configfile = acc.configfile
+
+            if configfile is None:
+                print('No configuration selected. Exit.')
+                exit()
+
+            ctrl = Controller(configfile)
+
+    elif sys.platform == 'linux':
+
+        app.use_app('glfw')
+        gloo.gl.use_gl('gl2')
+
+        configfile = None
+
+        if args.ini_file is not None:
+            configfile = args.ini_file
+
+        else:
+
+            acc.app = QtWidgets.QApplication([])
+            acc.main = StartupConfiguration()
+            acc.main.setup_ui()
+            acc.app.exec_()
+
+            configfile = acc.configfile
+
+        if configfile is None:
+            print('No configuration selected. Exit.')
+            exit()
+
+        ctrl = Controller(configfile)
+    else:
+        print('Sorry, probably not gonna work on \"{}\"'.format(sys.platform))
