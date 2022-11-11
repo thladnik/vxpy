5479b27da9403fb7aaa422c4825ad05dcbdb7b4f
tim <tim.hladnik@gmail.com>
2022-11-11 10:43:13 +0100

    Update setup and requirements;

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
 with open('requirements.txt', 'r') as f:
-    requirements = f.readlines()
+    install_deps = f.readlines()
 
 # Add wres~=1.0.3 for windows platform?
 
 # Add h5gview repository
 # requirements.append('h5gview @ git+https://git@github.com/thladnik/h5gview.git')
 
+toolchain_deps = install_deps + ['vxpy-tools @ git+https://git@github.com/thladnik/vxPy-tools.git@main']
+
 setup(
     name='vxpy',
     version='0.0.1',
+    python_requires='>=3.10',
     packages=find_packages(include=['vxpy', 'vxpy.*']),
     include_package_data=True,
     url='',
@@ -18,5 +21,6 @@ setup(
     author='Tim Hladnik',
     author_email='tim.hladnik@gmail.com',
     description='vxPy - Vision experiments in Python',
-    install_requires=requirements
+    install_requires=install_deps,
+    extras_require={'toolchain': toolchain_deps}
 )
