bb0ff600664caf752c3188854fa2f2af741e0bf1
thladnik <tim.hladnik@gmail.com>
2023-04-26 14:30:24 +0200

    Add git version determination for Windows platform
    Add a correct way to look up any git repository in the vxpy folder (for Windows) in order to add commit hash to version.

--- vxpy/__init__.py
+++ vxpy/__init__.py
@@ -34,13 +34,23 @@ try:
     # Import gitpython here, because dependency is not installed during setup
     import git
 
+    # Load repository (if available)
+    if sys.platform == 'linux':
+        path = os.path.join(*__path__[0].split(os.sep)[:-1])
+        repo = git.Repo(f'{os.sep}{path}')
+    elif sys.platform == 'win32':
+        parts = __path__[0].split(os.sep)
+        path = f'{parts[0]}\\{os.path.join(*parts[1:-1])}'
+        repo = git.Repo(path)
+    else:
+        raise NotImplementedError('No solution for this platform')
+
     # Get current HEAD
-    path = os.path.join(*__path__[0].split(os.sep)[:-1])
-    repo = git.Repo(f'{os.sep}{path}')
     commit_hash = repo.git.rev_parse('HEAD')
 
     # Append to version
     __version__ += f'-{commit_hash[:8]}'
+    print(__version__)
 
 except:
     pass
