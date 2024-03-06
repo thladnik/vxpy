89f4df2caffc1b33780b4fa973faff896ebede0d
localuser@setup <localuser@setup>
2024-03-06 11:13:23 +0100

    Fix auto-level adjustment during imageitem updates;

--- vxpy/extras/display_widgets.py
+++ vxpy/extras/display_widgets.py
@@ -1,19 +1,5 @@
 """
-vxPy ./gui/display/display_widgets.py
 Copyright (C) 2022 Tim Hladnik
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import importlib
 import inspect
@@ -395,7 +381,7 @@ class VisualStreamAddon(vxui.DisplayAddonWidget):
         if frame is None:
             return
 
-        self.graphics_widget.image_item.setImage(frame[0])
+        self.graphics_widget.image_item.setImage(frame[0], levels=[[0, 255], [0, 255], [0, 255]])
 
 
 class GraphicsWidget(pg.GraphicsLayoutWidget):
